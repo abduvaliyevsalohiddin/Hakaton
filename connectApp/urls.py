e6345b4921c1de33e351a3701147ffd09313ac44
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2024-04-28 02:38:10 -0700

    v c

--- connectApp/urls.py
+++ connectApp/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('create_connect/', ConnectionCreateAPIView.as_view()),
+    path('vacancies/', VacanciesAPIView.as_view())
 ]
--- connectApp/views.py
+++ connectApp/views.py
@@ -1,10 +1,11 @@
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework.generics import CreateAPIView
+from rest_framework.filters import SearchFilter
+from rest_framework.generics import CreateAPIView, ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import *
 
-from connectApp.models import Connection
-from connectApp.serializers import ConnectionSerializer
+from connectApp.models import Connection, Vacancy
+from connectApp.serializers import ConnectionSerializer, VacancySerializer
 from userApp.models import Profil
 
 
@@ -17,3 +18,12 @@ class ConnectionCreateAPIView(CreateAPIView):
         serializer.save(
             user1=self.request.user
         )
+
+
+class VacanciesAPIView(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = VacancySerializer
+    queryset = Vacancy.objects.all()
+
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description']
