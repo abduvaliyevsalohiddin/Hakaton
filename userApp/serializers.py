d42bd3dda48d14ee935934e49dc7547caf2e65cc
abduvaliyevsalohiddin <abduvaliyevsalohiddin568@gmail.com>
2024-04-27 20:54:09 -0700

    create user --> serializers

--- userApp/serializers.py
+++ userApp/serializers.py
@@ -6,3 +6,6 @@ class ProfilSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profil
         fields = '__all__'
+
+    def create(self, validated_data):
+        return Profil.objects.create_user(**validated_data)
